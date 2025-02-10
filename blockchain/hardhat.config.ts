0c36029b7b11d677a4bcbedfd9747792e73a9421
AlexDavid26 <alexbleotu2006@gmail.com>
2025-02-10 14:01:19 +0200

    Env

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -3,6 +3,14 @@ import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.28",
+    networks: {
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: process.env.WALLET_PRIVATE_KEY
+                ? [process.env.WALLET_PRIVATE_KEY]
+                : [],
+        },
+    },
 };
 
 export default config;
