ce73682a2bbbfd7847d1a404382d04495c5c3c8e
Alex Bleotu <alexbleotu2006@gmail.com>
2025-04-27 21:35:53 +0300

    Documentation (not finished)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { lingui } from "@lingui/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react({
