7dc1c660322781841e6f8cac3a2efe10bbdbc2ea
Alex Bleotu <alexbleotu2006@gmail.com>
2025-04-29 20:20:46 +0300

    Small padding change

--- frontend/src/pages/home/components/TopCampaigns.tsx
+++ frontend/src/pages/home/components/TopCampaigns.tsx
@@ -90,7 +90,7 @@ export function TopCampaigns() {
     }
 
     return (
-        <div className="py-16 bg-background-alt">
+        <div className="pt-4 pb-16 bg-background-alt">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <h2 className="text-3xl font-bold text-text mb-8">
                     {t`Top Active Campaigns`}
